573327f3a5a3d737e547e9e2ca346be1a7056dbc
Cody Henthorne <codyh.zbi@gmail.com>
2019-05-26 21:30:57 -0400

    update project structure

--- src/main.ino
+++ src/main.ino
@@ -4,7 +4,7 @@
 #include "TempMgr.h"
 #include "LiquidCrystal.h"
 
-#if PLATFORM_ID==10
+#if PLATFORM_ID == 10
 #include "cellular_hal.h"
 STARTUP(cellular_credentials_set("wholesale", "", "", NULL));
 #define ONEWIRE_PIN B0
@@ -23,63 +23,85 @@ LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
 
 TCPClient client;
 
-void setup() {
-   Serial.begin(9600);
-   Serial.println("Starting...");
-   temps.begin();
+void setup()
+{
+  Serial.begin(9600);
+  Serial.println("Starting...");
+  temps.begin();
 
-   // set up the LCD's number of columns and rows:
-   lcd.begin(16, 2);
-   // Print a message to the LCD.
-   lcd.print("Starting...");
+  // set up the LCD's number of columns and rows:
+  lcd.begin(16, 2);
+  // Print a message to the LCD.
+  lcd.print("Starting...");
 }
 
-void loop(void) {
+void loop(void)
+{
   printStatusMsg("Sample Round: " + String(sampleRounds));
 
   unsigned long start = millis();
 
-  for (int i = 0; i < numAddresses; i++) {
-    if (temps.requestTemperaturesByAddress(addresses[i])) {
+  for (int i = 0; i < numAddresses; i++)
+  {
+    if (temps.requestTemperaturesByAddress(addresses[i]))
+    {
       float temp = temps.getTempF(addresses[i]);
-      if (validTemp(temp)) {
-        Serial.print("Addr "); Serial.print(i); Serial.print(" "); Serial.println(temp);
+      if (validTemp(temp))
+      {
+        Serial.print("Addr ");
+        Serial.print(i);
+        Serial.print(" ");
+        Serial.println(temp);
         tempMgr.addTempSample(i, temp);
         lcd.setCursor(0, 1);
         lcd.print("Temp[" + String(i) + "]: " + String(temp));
-      } else {
-        Serial.print("Addr "); Serial.print(i); Serial.print(" "); Serial.println("Invalid Temp");
+      }
+      else
+      {
+        Serial.print("Addr ");
+        Serial.print(i);
+        Serial.print(" ");
+        Serial.println("Invalid Temp");
       }
     }
   }
   unsigned long end = millis();
 
   sampleRounds++;
-  if (sampleRounds == 20) {
+  if (sampleRounds == 20)
+  {
     sendData();
     sampleRounds = 0;
   }
 
   unsigned long delayMS = 3000;
-  if (end > start) {
+  if (end > start)
+  {
     delayMS = max(1000, delayMS - (end - start));
   }
   delay(delayMS);
 }
 
-bool validTemp(float temp) {
+bool validTemp(float temp)
+{
   return (DEVICE_DISCONNECTED_F != temp && temp > 0 && temp < 130);
 }
 
-void sendData() {
+void sendData()
+{
   int tries = 0;
   String data = "";
+  unsigned int datum = 0;
+  String fullData = "";
 
   printStatusMsg("Prearing data...");
-  for (int i = 0; i < numAddresses; i++) {
+  for (int i = 0; i < numAddresses; i++)
+  {
     float temp = tempMgr.getAverageTemp(i);
-    if (temp != NO_DATA) {
-      if (data.length() > 0) {
+    if (temp != NO_DATA)
+    {
+      if (data.length() > 0)
+      {
         data += "&";
       }
       data += "field" + String(i) + "=" + String(temp);
@@ -87,12 +109,15 @@ void sendData() {
   }
 
   lcd.setCursor(0, 0);
-  Serial.print("Sending data: "); Serial.println(data);
-  while (tries < 3) {
+  Serial.print("Sending data: ");
+  Serial.println(data);
+  while (tries < 3)
+  {
     tries++;
 
     printStatusMsg("Connecting...");
-    if (client.connect("api.thingspeak.com", 80)) {
+    if (client.connect("api.thingspeak.com", 80))
+    {
       Serial.println("connected");
       printStatusMsg("Connected!");
       client.println("POST /update HTTP/1.0");
@@ -105,13 +130,20 @@ void sendData() {
       client.println();
       client.flush();
       Serial.println("sending data: " + data);
-      while (client.read() != -1 && client.connected()) {}
+      datum = client.read();
+      while (datum != -1 && client.connected())
+      {
+        datum = client.read();
+      }
+      delay(3000);
       client.stop();
       Serial.println("data sent");
       lcd.setCursor(0, 0);
       lcd.print("Data sent");
       break;
-    } else {
+    }
+    else
+    {
       printStatusMsg("Connect failed!");
       Serial.println("connection failed");
       delay(2000);
@@ -119,7 +151,8 @@ void sendData() {
   }
 }
 
-void printStatusMsg(String msg) {
+void printStatusMsg(String msg)
+{
   lcd.clear();
   lcd.setCursor(0, 0);
   lcd.print(msg);
